ac6f1af3711ff8edd9a48345ceaeb11f15078501
kkung <kkungkkung@gmail.com>
2013-01-15 20:20:37 +0900

    welcome 2013 lunar year's day

--- runrun.sh
+++ runrun.sh
@@ -2,4 +2,9 @@ HOME=/Users/kkung/private/chooseok
 
 source $HOME/env/bin/activate
 echo "gogogo"
-python gohome.py data_0930.json data_1001.json data_0929_1001.json &
+for i in $(seq 1 5)
+do
+    python gohome.py lunar.json &
+done
+echo "waiting.."
+wait
