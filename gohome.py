dc5f7f33d01cc514794489337b9188c5848ee162
kkung <kkungkkung@gmail.com>
2013-01-15 20:05:49 +0900

    enhanced performance

--- gohome.py
+++ gohome.py
@@ -1,27 +1,39 @@
 #-*- encoding: utf-8 -*-
-
+import gevent
+from gevent import monkey; monkey.patch_all()
 import json, urllib2, socket, re, sys
 from contextlib import closing
 
-import re
 __RE_RESULT__ = re.compile("<b>([^<]*)</b>", re.I | re.S | re.M)
 
-socket.setdefaulttimeout(120)
+def go(fname):
+    socket.setdefaulttimeout(120)
 
-data = json.load(open(sys.argv[1]))
-dr = data['request']
-req = urllib2.Request(dr['url'])
+    data = json.load(open(fname))
+    dr = data['request']
+    req = urllib2.Request(dr['url'])
 
-for h in dr['headers']:
-	req.add_header(h['name'],  h['value'])
+    for h in dr['headers']:
+        req.add_header(h['name'],  h['value'])
 
-req.data = dr['postData']['text']
+    req.data = dr['postData']['text']
 
-fp = urllib2.urlopen(req, )
-with closing(fp):
-	result = fp.read().decode('cp949')
+    fp = urllib2.urlopen(req, )
+    with closing(fp):
+        result = fp.read().decode('cp949')
 
-print 
-for m in __RE_RESULT__.findall(result):
-	if len(m.strip()) > 0:
-		print u'[%s] %s' % (sys.argv[1], m)
+    print
+    for m in __RE_RESULT__.findall(result):
+        if len(m.strip()) > 0:
+            print u'[%s] %s' % (fname, m)
+
+if __name__ == '__main__':
+
+    glets = []
+    for fs in sys.argv[1:]:
+        for i in range(5):
+            print '[%s] Spawn %d' % (fs, i)
+            glets.append(gevent.spawn(go, fs))
+
+    gevent.joinall(glets)
+
--- runrun.sh
+++ runrun.sh
@@ -1,7 +1,5 @@
 HOME=/Users/kkung/private/chooseok
 
 source $HOME/env/bin/activate
-python gohome.py data_0930.json &
-python gohome.py data_1001.json &
-python gohome.py data_0929_1001.json &
 echo "gogogo"
+python gohome.py data_0930.json data_1001.json data_0929_1001.json &
